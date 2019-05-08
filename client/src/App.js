b73a96b18132109c0a858deecc8305b9b71cda5f
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-05-08 10:58:46 +0530

    PrivateRoute Set-up: Complete

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import PrivateRoute from "./components/dashboard/privateRoute/PrivateRoute";
+import PrivateRoute from "./components/dashboard/privateroute/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 
 // Check for token to keep user logged in
