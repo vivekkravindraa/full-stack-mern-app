c7c34a1e03b842570522839f412ea9f45fd6a1b5
Vivek Ravindra <vivekravindra@Viveks-MacBook-Air.local>
2021-08-29 11:50:46 +0530

    fix: update outdated npm packages

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,9 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {    
+    secretOrKey: 'yoursecretkeyishere',
+    mongoURI: 'mongodb://localhost:27017/testdb'
+};
