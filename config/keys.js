77fc17d085b7553c8f2bb4c66fc36e28ec2f45e6
Vivek Ravindra <vivekravindra@Viveks-MacBook-Air.local>
2021-08-29 17:06:33 +0530

    fix: add atlas sandbox

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {    
     secretOrKey: 'yoursecretkeyishere',
-    mongoURI: 'mongodb://localhost:27017/testdb'
+    // mongoURI: `mongodb://localhost:27017/sample_db`,
+    mongoURI: `mongodb+srv://vivekkravindraa:${process.env.DB_PASSWORD}@cluster0.pswyb.mongodb.net/sample_db`
 };
