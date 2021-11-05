34cde1bc146700a6b27bb18288d4e4b66953c43f
Vivek Ravindra <vivekravindra@Viveks-MacBook-Air.local>
2021-11-05 18:36:18 +0530

    init vercel

--- server.js
+++ server.js
@@ -43,7 +43,13 @@ app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    // app.use(express.static('client/build'));
+    const path = require('path');
+
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
